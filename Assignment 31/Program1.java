52a0a6fd8434dd45f59aeaa5d8320b11fdad0b4a
Prathamesh Rajesh Bawane <116380431+Pratham1801@users.noreply.github.com>
2025-12-01 11:40:37 +0530

    Add files via upload

---
+++ Assignment 31/Program1.java
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Import Files
+//
+////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Pattern
+//  Description     :   Used to Print the Pattern
+//  Input           :   Integer, Integer
+//  Output          :   Pattern
+//  Author          :   Prathamesh Rajesh Bawane
+//  Date            :   23/11/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+class Program1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        pobj = null;
+        sobj.close();
+
+        System.gc();
+    }
+}
+
---
+++ Assignment 31/Program_4.java
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Import Files
+//
+////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Pattern
+//  Description     :   Used To Print Pattern
+//  Input           :   Integer, Integer
+//  Output          :   Pattern
+//  Author          :   Prathamesh Rajesh Bawane
+//  Date            :   01/12/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(j == iCol || i == iRow || i == 1 || j == 1 || i == j)
+                {
+                    System.out.printf("*\t");
+                }
+                else
+                {
+                    System.out.printf(" \t")
+                }
+            }
+            System.out.printf("\n");
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+class Program_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the number of Rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        pobj = null;
+        sobj.close();
+
+        System.gc();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Test Case Handled By Application
+//  
+//  Input   :   iRow : 4        iCol : 4
+//  
+//  Output  :   *   *   *   *
+//              *   *       *
+//              *       *   *
+//              *   *   *   *
+////////////////////////////////////////////////////////////////////////
